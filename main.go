a529d8eaee40d58ba4fbe3a637ffb468d2cfb17c
sikbrad <sikbrad@gmail.com>
2021-01-03 17:10:44 +0900

    packaging2

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package gony
 
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("gony main started")
-
-	//zeros := gony.Zeros(5,3)
-	//gony.PrintMatrix(zeros, "zeros")
-
-
-	fmt.Println("gony main finished")
-}
+//func main() {
+//	fmt.Println("gony main started")
+//
+//	//zeros := gony.Zeros(5,3)
+//	//gony.PrintMatrix(zeros, "zeros")
+//
+//
+//	fmt.Println("gony main finished")
+//}
 
